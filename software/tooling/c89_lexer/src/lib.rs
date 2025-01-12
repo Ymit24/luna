a740e5edd86e29cf92a2a5ba7a55a3ab1384673c
Christian <christianryansmith@gmail.com>
2025-01-11 22:40:07 -0600

    wip: update lexical tokens

--- software/tooling/c89_lexer/src/lib.rs
+++ software/tooling/c89_lexer/src/lib.rs
@@ -4,28 +4,98 @@ use chumsky::prelude::*;
 use token::Token;
 
 pub fn lexer() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
-    let base_keywords = [
-        "auto", "double", "int", "struct", "break", "else", "long", "switch", "case", "enum",
-        "register", "typedef", "char", "extern", "return", "union", "const", "float", "short",
-        "unsigned", "continue", "for", "signed", "void", "default", "goto", "sizeof", "volatile",
-        "do", "if", "static", "while",
-    ];
     let operators = [
         "[", "]", "(", ")", ".", "->", "++", "--", "&", "*", "+", "-", "~", "!", "sizeof", "/",
         "%", "<<", ">>", "<", ">", "<=", ">=", "==", "!=", "^", "|", "&&", "||", "?", ":", "=",
         "*=", "/=", "%=", "+=", "-=", "<<=", ">>=", "&=", "^=", "|=", ",", "#", "##",
     ];
 
-    let punctuators = [
-        "[", "]", "(", ")", "{", "}", "*", ",", ":", "=", ";", "...", "#",
-    ];
+    let keyword = choice([
+        text::keyword("auto").to(Token::Auto),
+        text::keyword("Auto").to(Token::Auto),
+        text::keyword("double").to(Token::Double),
+        text::keyword("int").to(Token::Int),
+        text::keyword("struct").to(Token::Struct),
+        text::keyword("break").to(Token::Break),
+        text::keyword("else").to(Token::Else),
+        text::keyword("long").to(Token::Long),
+        text::keyword("switch").to(Token::Switch),
+        text::keyword("case").to(Token::Case),
+        text::keyword("enum").to(Token::Enum),
+        text::keyword("register").to(Token::Register),
+        text::keyword("typedef").to(Token::Typedef),
+        text::keyword("char").to(Token::Char),
+        text::keyword("extern").to(Token::Extern),
+        text::keyword("return").to(Token::Return),
+        text::keyword("union").to(Token::Union),
+        text::keyword("const").to(Token::Const),
+        text::keyword("float").to(Token::Float),
+        text::keyword("short").to(Token::Short),
+        text::keyword("unsigned").to(Token::Unsigned),
+        text::keyword("continue").to(Token::Continue),
+        text::keyword("for").to(Token::For),
+        text::keyword("signed").to(Token::Signed),
+        text::keyword("void").to(Token::Void),
+        text::keyword("default").to(Token::Default),
+        text::keyword("goto").to(Token::Goto),
+        text::keyword("sizeof").to(Token::Sizeof),
+        text::keyword("volatile").to(Token::Volatile),
+        text::keyword("do").to(Token::Do),
+        text::keyword("if").to(Token::If),
+        text::keyword("static").to(Token::Static),
+        text::keyword("while").to(Token::While),
+    ]);
 
-    let keyword = choice(
-        base_keywords
-            .map(|keyword| text::keyword(keyword).map(|_| Token::Keyword(keyword.to_string()))),
-    );
-    let operator = choice(operators.map(|operator| just(operator).to(Token::Operator)));
-    let punctuator = choice(punctuators.map(|punctuator| just(punctuator).to(Token::Punctuator)));
+    let operator = choice([
+        just("[").to(Token::LeftBracket),
+        just("]").to(Token::RightBracket),
+        just("(").to(Token::LeftParen),
+        just(")").to(Token::RightParen),
+        just("{").to(Token::LeftBrace),
+        just("}").to(Token::RightBrace),
+        just(".").to(Token::Dot),
+        just("->").to(Token::Arrow),
+        just("++").to(Token::PlusPlus),
+        just("--").to(Token::MinusMinus),
+        just("&&").to(Token::AndAnd),
+        just("&").to(Token::Ampersand),
+        just("*").to(Token::Asterisk),
+        just("+").to(Token::Plus),
+        just("-").to(Token::Minus),
+        just("~").to(Token::Tilde),
+        just("!").to(Token::Bang),
+        just("/").to(Token::Slash),
+        just("%").to(Token::Percent),
+        just("<<").to(Token::LeftShift),
+        just(">>").to(Token::RightShift),
+        just("<").to(Token::Less),
+        just(">").to(Token::Greater),
+        just("<=").to(Token::LessEqual),
+        just(">=").to(Token::GreaterEqual),
+        just("==").to(Token::EqualEqual),
+        just("!=").to(Token::NotEqual),
+        just("^").to(Token::Caret),
+        just("|").to(Token::Pipe),
+        just("||").to(Token::OrOr),
+        just("?").to(Token::Question),
+        just(":").to(Token::Colon),
+        just(";").to(Token::Semicolon),
+        just("*=").to(Token::AsteriskEqual),
+        just("/=").to(Token::SlashEqual),
+        just("%=").to(Token::PercentEqual),
+        just("+=").to(Token::PlusEqual),
+        just("-=").to(Token::MinusEqual),
+        just("<<=").to(Token::LeftShiftEqual),
+        just(">>=").to(Token::RightShiftEqual),
+        just("&=").to(Token::AmpersandEqual),
+        just("^=").to(Token::CaretEqual),
+        just("|=").to(Token::PipeEqual),
+        just(",").to(Token::Comma),
+        just("=").to(Token::Equal),
+        just("#").to(Token::Hash),
+        just("##").to(Token::HashHash),
+        just("...").to(Token::Ellipsis),
+    ]);
 
     let identifier = text::ident().map(Token::Identifier);
 
@@ -35,17 +105,10 @@ pub fn lexer() -> impl Parser<char, Vec<Token>, Error = Simple<char>> {
         .delimited_by(just('"'), just('"'))
         .map(|_| Token::StringLiteral);
 
-    choice((
-        keyword,
-        identifier,
-        constant,
-        string_literal,
-        punctuator,
-        operator,
-    ))
-    .padded()
-    .repeated()
-    .then_ignore(end())
+    choice((keyword, identifier, constant, string_literal, operator))
+        .padded()
+        .repeated()
+        .then_ignore(end())
 }
 
 #[cfg(test)]
@@ -60,11 +123,11 @@ mod test {
         assert_eq!(
             results,
             Ok(vec![
-                Token::Keyword("auto".into()),
-                Token::Keyword("double".into()),
-                Token::Keyword("for".into()),
-                Token::Keyword("signed".into()),
-                Token::Keyword("void".into())
+                Token::Auto,
+                Token::Double,
+                Token::For,
+                Token::Signed,
+                Token::Void
             ])
         );
     }
@@ -109,13 +172,7 @@ mod test {
         let results = lexer().parse(source);
         assert_eq!(
             results,
-            Ok(vec![
-                Token::Operator,
-                Token::Operator,
-                Token::Operator,
-                Token::Operator,
-                Token::Operator
-            ])
+            Ok(vec![Token::Plus, Token::Equal, Token::Minus, Token::AndAnd,])
         );
     }
 
--- software/tooling/c89_lexer/src/token.rs
+++ software/tooling/c89_lexer/src/token.rs
@@ -1,11 +1,92 @@
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Token {
-    Keyword(String),
     Identifier(String),
     Constant,
     StringLiteral,
-    Operator,
-    Punctuator,
+
+    // Keywords
+    Auto,
+    Double,
+    Int,
+    Struct,
+    Break,
+    Else,
+    Long,
+    Switch,
+    Case,
+    Enum,
+    Register,
+    Typedef,
+    Char,
+    Extern,
+    Return,
+    Union,
+    Const,
+    Float,
+    Short,
+    Unsigned,
+    Continue,
+    For,
+    Signed,
+    Void,
+    Default,
+    Goto,
+    Sizeof,
+    Volatile,
+    Do,
+    If,
+    Static,
+    While,
+
+    // Operators
+    LeftBracket,     // [
+    RightBracket,    // ]
+    LeftBrace,       // {
+    RightBrace,      // }
+    LeftParen,       // (
+    RightParen,      // )
+    Dot,             // .
+    Arrow,           // ->
+    PlusPlus,        // ++
+    MinusMinus,      // --
+    Ampersand,       // &
+    Asterisk,        // *
+    Plus,            // +
+    Minus,           // -
+    Tilde,           // ~
+    Bang,            // !
+    Slash,           // /
+    Percent,         // %
+    LeftShift,       // <<
+    RightShift,      // >>
+    Less,            // <
+    Greater,         // >
+    LessEqual,       // <=
+    GreaterEqual,    // >=
+    EqualEqual,      // ==
+    NotEqual,        // !=
+    Caret,           // ^
+    Pipe,            // |
+    AndAnd,          // &&
+    OrOr,            // ||
+    Question,        // ?
+    Colon,           // :
+    Semicolon,       // ;
+    Equal,           // =
+    AsteriskEqual,   // *=
+    SlashEqual,      // /=
+    PercentEqual,    // %=
+    PlusEqual,       // +=
+    MinusEqual,      // -=
+    LeftShiftEqual,  // <<=
+    RightShiftEqual, // >>=
+    AmpersandEqual,  // &=
+    CaretEqual,      // ^=
+    PipeEqual,       // |=
+    Comma,           // ,
+    Hash,            // #
+    HashHash,        // ##
+    Ellipsis,        // ...
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
