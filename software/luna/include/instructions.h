ad9eb6715b555c0a22708b9c4bd9d48a16c79199
Christian Smith <christianryansmith@gmail.com>
2025-07-03 07:52:26 -0500

    feat: adding instruction builder

---
+++ software/luna/include/instructions.h
@@ -0,0 +1,36 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+#include "luna_string.h"
+#include <stdio.h>
+struct InstructionBuilder {
+  FILE *output_file;
+};
+
+struct InstructionBuilder instruction_builder_make(FILE *output_file);
+
+void instr_write_line(struct InstructionBuilder *instruction_builder,
+                      struct LunaString line);
+
+extern uint16_t IB_SP_ADDR, IB_BSP_ADDR, IB_NP_ADDR;
+
+// push $1 $2
+void instr_push_push(struct InstructionBuilder *instruction_builder,
+                     struct LunaString memory_segment, uint16_t location);
+// pop $1 $2
+void instr_push_pop(struct InstructionBuilder *instruction_builder,
+                    struct LunaString memory_segment, uint16_t location);
+// // add
+// void instr_push_add(struct InstructionBuilder *instruction_builder);
+// // sub
+// void instr_push_sub(struct InstructionBuilder *instruction_builder);
+// // $main
+// void instr_push_label(struct InstructionBuilder *instruction_builder,
+//                       struct LunaString label);
+// // ret
+// void instr_push_return(struct InstructionBuilder *instruction_builder);
+// // call $1
+// void instr_push_call(struct InstructionBuilder *instruction_builder,
+//                      struct LunaString label);
+
+#endif
