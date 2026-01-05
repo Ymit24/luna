8e17aade4d4947de22311a018ddaba6067ebf08d
Christian Smith <chris@Christians-MacBook-Air.local>
2026-01-05 14:57:25 -0600

    feat: added query function

--- software/luna/main.c
+++ software/luna/main.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "annotator.h"
@@ -14,6 +15,490 @@
 #include "parser.h"
 #include "token.h"
 #include "llvm-c/Core.h"
+
+struct LineMap {
+  uint16_t *line_offsets;
+  uint16_t num_lines;
+};
+
+enum ContainingNodeType {
+  NODE_NONE,
+  NODE_MODULE,
+  NODE_FUNCTION,
+  NODE_VARIABLE,
+  NODE_EXPRESSION,
+};
+
+struct ContainingNode {
+  enum ContainingNodeType type;
+  struct SourceLocation location;
+  struct LunaString name;
+  union {
+    struct ModuleNode *module;
+    struct FunctionDefinitionExpressionNode *function;
+    struct DeclarationStatementNode *variable;
+    struct ExpressionNode *expression;
+  } node;
+};
+
+static struct LineMap build_line_map(struct LunaString source,
+                                     struct ArenaAllocator *alloc) {
+  uint16_t line_count = 1;
+  for (uint16_t i = 0; i < source.length; i++) {
+    if (source.data[i] == '\n') {
+      line_count++;
+    }
+  }
+
+  uint16_t *offsets = arena_alloc(alloc, sizeof(uint16_t) * line_count);
+  offsets[0] = 0;
+  uint16_t current_line = 1;
+  for (uint16_t i = 0; i < source.length; i++) {
+    if (source.data[i] == '\n') {
+      offsets[current_line++] = i + 1;
+    }
+  }
+
+  return (struct LineMap){.line_offsets = offsets, .num_lines = line_count};
+}
+
+static uint16_t line_col_to_byte_offset(struct LineMap map, uint16_t line,
+                                        uint16_t col,
+                                        struct LunaString source) {
+  if (line >= map.num_lines) {
+    return source.length;
+  }
+  uint16_t offset = map.line_offsets[line] + col;
+  if (offset > source.length) {
+    return source.length;
+  }
+  return offset;
+}
+
+static bool position_in_span(uint16_t pos, struct SourceLocation loc) {
+  return loc.start <= pos && pos <= loc.end;
+}
+
+static bool should_update_containing_node(uint16_t pos,
+                                        struct SourceLocation new_loc,
+                                        struct SourceLocation current_best_loc) {
+  if (current_best_loc.start == 0 && current_best_loc.end == 0) {
+    return true;
+  }
+  uint16_t new_span = new_loc.end - new_loc.start;
+  uint16_t current_span = current_best_loc.end - current_best_loc.start;
+  return new_span < current_span && position_in_span(pos, new_loc);
+}
+
+static void find_containing_in_expression(struct ExpressionNode *expr,
+                                         uint16_t pos,
+                                         struct ContainingNode *result);
+static void find_containing_in_statement(struct FunctionStatementNode *stmt,
+                                       uint16_t pos,
+                                       struct ContainingNode *result);
+static void find_containing_in_module_statement(
+    struct ModuleStatementNode *stmt, uint16_t pos,
+    struct ContainingNode *result);
+
+static void find_containing_in_expression(struct ExpressionNode *expr,
+                                         uint16_t pos,
+                                         struct ContainingNode *result) {
+  if (!expr) {
+    return;
+  }
+
+  if (!position_in_span(pos, expr->location)) {
+    return;
+  }
+
+  switch (expr->type) {
+  case EXPR_FN_DEF: {
+    if (should_update_containing_node(pos, expr->location, result->location)) {
+      result->type = NODE_FUNCTION;
+      result->location = expr->location;
+      result->node.function = expr->node.fn_def;
+      result->name = string_make((char *)"<function>");
+    }
+    if (expr->node.fn_def->body) {
+      struct FunctionStatementNode *stmt = expr->node.fn_def->body;
+      while (stmt) {
+        find_containing_in_statement(stmt, pos, result);
+        stmt = stmt->next;
+      }
+    }
+    break;
+  }
+  case EXPR_MOD_DEF: {
+    if (should_update_containing_node(pos, expr->location, result->location)) {
+      result->type = NODE_MODULE;
+      result->location = expr->location;
+      result->node.module = expr->node.module_definition;
+      result->name = string_make((char *)"<module>");
+    }
+    if (expr->node.module_definition->statements) {
+      struct ModuleStatementNode *stmt = expr->node.module_definition->statements;
+      while (stmt) {
+        find_containing_in_module_statement(stmt, pos, result);
+        stmt = stmt->next;
+      }
+    }
+    break;
+  }
+  case EXPR_SYMBOL_LITERAL:
+  case EXPR_INTEGER_LITERAL:
+  case EXPR_STRING_LITERAL:
+  case EXPR_BINARY:
+  case EXPR_REF:
+  case EXPR_DEREF:
+  case EXPR_NOT:
+  case EXPR_CAST:
+  case EXPR_STRUCT_DEF:
+  case EXPR_STRUCT_INIT:
+  case EXPR_FIELD_ACCESS:
+  case EXPR_ARRAY_INITIALIZER:
+  case EXPR_FN_CALL:
+  case EXPR_VALUESIZE:
+  case EXPR_TYPESIZE: {
+    if (should_update_containing_node(pos, expr->location, result->location)) {
+      result->type = NODE_EXPRESSION;
+      result->location = expr->location;
+      result->node.expression = expr;
+      const char *type_name = "unknown";
+      switch (expr->type) {
+      case EXPR_SYMBOL_LITERAL:
+        type_name = "symbol";
+        break;
+      case EXPR_INTEGER_LITERAL:
+        type_name = "integer";
+        break;
+      case EXPR_STRING_LITERAL:
+        type_name = "string";
+        break;
+      case EXPR_BINARY:
+        type_name = "binary";
+        break;
+      case EXPR_REF:
+        type_name = "ref";
+        break;
+      case EXPR_DEREF:
+        type_name = "deref";
+        break;
+      case EXPR_NOT:
+        type_name = "not";
+        break;
+      case EXPR_CAST:
+        type_name = "cast";
+        break;
+      case EXPR_FN_CALL:
+        type_name = "call";
+        break;
+      case EXPR_STRUCT_DEF:
+        type_name = "struct_def";
+        break;
+      case EXPR_STRUCT_INIT:
+        type_name = "struct_init";
+        break;
+      case EXPR_FIELD_ACCESS:
+        type_name = "field_access";
+        break;
+      case EXPR_ARRAY_INITIALIZER:
+        type_name = "array";
+        break;
+      case EXPR_VALUESIZE:
+        type_name = "valuesize";
+        break;
+      case EXPR_TYPESIZE:
+        type_name = "typesize";
+        break;
+      default:
+        type_name = "unknown";
+        break;
+      }
+      result->name = string_make((char *)type_name);
+    }
+    break;
+  }
+  }
+}
+
+static void find_containing_in_statement(struct FunctionStatementNode *stmt,
+                                       uint16_t pos,
+                                       struct ContainingNode *result) {
+  if (!stmt) {
+    return;
+  }
+
+  if (!position_in_span(pos, stmt->location)) {
+    return;
+  }
+
+  switch (stmt->type) {
+  case FN_STMT_LET:
+  case FN_STMT_CONST:
+  case FN_STMT_ASSIGN: {
+    struct DeclarationStatementNode *decl =
+        (stmt->type == FN_STMT_LET || stmt->type == FN_STMT_CONST)
+            ? stmt->node.decl
+            : NULL;
+    if (decl && decl->expression) {
+      find_containing_in_expression(decl->expression, pos, result);
+    }
+    if (decl && should_update_containing_node(pos, stmt->location,
+                                             result->location)) {
+      result->type = NODE_VARIABLE;
+      result->location = stmt->location;
+      result->node.variable = decl;
+      result->name = decl->symbol;
+    }
+    break;
+  }
+  case FN_STMT_RETURN:
+    if (stmt->node.ret && stmt->node.ret->expression) {
+      find_containing_in_expression(stmt->node.ret->expression, pos, result);
+    }
+    break;
+  case FN_STMT_FN_CALL:
+    if (stmt->node.fn_call) {
+      struct FunctionCallExpressionNode *call = stmt->node.fn_call;
+      if (call->arguments) {
+        struct FunctionCallArgumentExpressionsNode *arg = call->arguments;
+        while (arg) {
+          find_containing_in_expression(arg->argument, pos, result);
+          arg = arg->next;
+        }
+      }
+    }
+    break;
+  case FN_STMT_IF:
+    if (stmt->node.if_stmt->condition) {
+      find_containing_in_expression(stmt->node.if_stmt->condition, pos, result);
+    }
+    if (stmt->node.if_stmt->body) {
+      struct FunctionStatementNode *s = stmt->node.if_stmt->body;
+      while (s) {
+        find_containing_in_statement(s, pos, result);
+        s = s->next;
+      }
+    }
+    if (stmt->node.if_stmt->next) {
+      struct IfStatementNode *else_chain = stmt->node.if_stmt->next;
+      while (else_chain) {
+        if (else_chain->condition) {
+          find_containing_in_expression(else_chain->condition, pos, result);
+        }
+        if (else_chain->body) {
+          struct FunctionStatementNode *s = else_chain->body;
+          while (s) {
+            find_containing_in_statement(s, pos, result);
+            s = s->next;
+          }
+        }
+        else_chain = else_chain->next;
+      }
+    }
+    break;
+  case FN_STMT_WHILE:
+    if (stmt->node.while_stmt->condition) {
+      find_containing_in_expression(stmt->node.while_stmt->condition, pos, result);
+    }
+    if (stmt->node.while_stmt->body) {
+      struct FunctionStatementNode *s = stmt->node.while_stmt->body;
+      while (s) {
+        find_containing_in_statement(s, pos, result);
+        s = s->next;
+      }
+    }
+    break;
+  }
+}
+
+static void find_containing_in_module_statement(
+    struct ModuleStatementNode *stmt, uint16_t pos,
+    struct ContainingNode *result) {
+  if (!stmt) {
+    return;
+  }
+
+  if (!position_in_span(pos, stmt->location)) {
+    return;
+  }
+
+  switch (stmt->type) {
+  case MOD_STMT_LET:
+  case MOD_STMT_CONST: {
+    struct DeclarationStatementNode *decl = stmt->node.decl;
+    if (decl && decl->expression) {
+      find_containing_in_expression(decl->expression, pos, result);
+    }
+    if (decl && should_update_containing_node(pos, stmt->location,
+                                             result->location)) {
+      result->type = NODE_VARIABLE;
+      result->location = stmt->location;
+      result->node.variable = decl;
+      result->name = decl->symbol;
+    }
+    break;
+  }
+  }
+}
+
+static struct ContainingNode
+find_smallest_containing_node(struct ModuleStatementNode *ast,
+                                 uint16_t byte_offset) {
+  struct ContainingNode result = {0};
+  struct ModuleStatementNode *stmt = ast;
+  while (stmt) {
+    find_containing_in_module_statement(stmt, byte_offset, &result);
+    stmt = stmt->next;
+  }
+  return result;
+}
+
+static const char *node_type_to_string(enum ContainingNodeType type) {
+  switch (type) {
+  case NODE_NONE:
+    return "none";
+  case NODE_MODULE:
+    return "module";
+  case NODE_FUNCTION:
+    return "function";
+  case NODE_VARIABLE:
+    return "variable";
+  case NODE_EXPRESSION:
+    return "expression";
+  default:
+    return "unknown";
+  }
+}
+
+static void test_cursor_location(struct ModuleStatementNode *ast,
+                                struct LunaString source, uint16_t line,
+                                uint16_t col) {
+  uint8_t arena_buffer[UINT16_MAX];
+  struct ArenaAllocator local_alloc = arena_make(arena_buffer, sizeof(arena_buffer));
+  struct LineMap map = build_line_map(source, &local_alloc);
+
+  uint16_t line_0based = line;
+  uint16_t col_0based = col;
+
+  if (line_0based >= map.num_lines) {
+    printf("Error: Line %d out of range (0-%d)\n", line_0based,
+           map.num_lines - 1);
+    return;
+  }
+
+  uint16_t byte_offset =
+      line_col_to_byte_offset(map, line_0based, col_0based, source);
+
+  printf("\n=== Cursor Location Query ===\n");
+  printf("Cursor: line=%d, col=%d, byte_offset=%d\n", line_0based,
+         col_0based, byte_offset);
+
+  struct ContainingNode result =
+      find_smallest_containing_node(ast, byte_offset);
+
+  printf("\n--- Result ---\n");
+
+  if (result.type == NODE_NONE) {
+    printf("No node found at cursor location\n");
+    return;
+  }
+
+  printf("Type: %s\n", node_type_to_string(result.type));
+  printf("Span: %d-%d (length: %d)\n", result.location.start,
+         result.location.end,
+         result.location.end - result.location.start);
+
+  if (result.name.length > 0) {
+    printf("Name: %.*s\n", (int)result.name.length, result.name.data);
+  }
+
+  switch (result.type) {
+  case NODE_FUNCTION:
+    if (result.node.function && result.node.function->body) {
+      printf("Function body has statements\n");
+    }
+    break;
+  case NODE_MODULE:
+    if (result.node.module) {
+      printf("Module has statements\n");
+    }
+    break;
+  case NODE_VARIABLE:
+    if (result.node.variable) {
+      printf("is_const: %s\n", result.node.variable->is_const ? "true" : "false");
+      printf("has_type: %s\n",
+             result.node.variable->has_type ? "true" : "false");
+    }
+    break;
+  case NODE_EXPRESSION:
+    if (result.node.expression) {
+      const char *expr_type = "unknown";
+      switch (result.node.expression->type) {
+      case EXPR_SYMBOL_LITERAL:
+        expr_type = "symbol";
+        break;
+      case EXPR_INTEGER_LITERAL:
+        expr_type = "integer literal";
+        break;
+      case EXPR_STRING_LITERAL:
+        expr_type = "string literal";
+        break;
+      case EXPR_BINARY:
+        expr_type = "binary expression";
+        break;
+      case EXPR_FN_CALL:
+        expr_type = "function call";
+        break;
+      case EXPR_FN_DEF:
+        expr_type = "function definition";
+        break;
+      case EXPR_MOD_DEF:
+        expr_type = "module definition";
+        break;
+      case EXPR_REF:
+        expr_type = "reference";
+        break;
+      case EXPR_DEREF:
+        expr_type = "dereference";
+        break;
+      case EXPR_NOT:
+        expr_type = "not";
+        break;
+      case EXPR_CAST:
+        expr_type = "cast";
+        break;
+      case EXPR_STRUCT_DEF:
+        expr_type = "struct definition";
+        break;
+      case EXPR_STRUCT_INIT:
+        expr_type = "struct initialization";
+        break;
+      case EXPR_FIELD_ACCESS:
+        expr_type = "field access";
+        break;
+      case EXPR_ARRAY_INITIALIZER:
+        expr_type = "array initializer";
+        break;
+      case EXPR_VALUESIZE:
+        expr_type = "valuesize";
+        break;
+      case EXPR_TYPESIZE:
+        expr_type = "typesize";
+        break;
+      default:
+        expr_type = "unknown";
+        break;
+      }
+      printf("Expression type: %s\n", expr_type);
+    }
+    break;
+  default:
+    break;
+  }
+
+  printf("=== End Query ===\n");
+}
 
 static void print_indent(int indent) {
   for (int i = 0; i < indent; i++) {
@@ -400,7 +885,9 @@ struct LunaString read_file(const char *path, struct ArenaAllocator *alloc) {
 }
 
 struct ModuleStatementNode *parse_source_file(struct ArenaAllocator *allocator,
-                                              char *source_file) {
+                                               char *source_file,
+                                               uint16_t cursor_line,
+                                               uint16_t cursor_col) {
 
   struct LunaString src = read_file(source_file, allocator);
   struct LunaString filename = string_make(source_file);
@@ -430,6 +917,11 @@ struct ModuleStatementNode *parse_source_file(struct ArenaAllocator *allocator,
   puts("done parsing");
   puts("\n\nAST Structure:");
   print_module_statement_list(stmt, 0);
+
+  if (cursor_line != (uint16_t)-1 && cursor_col != (uint16_t)-1) {
+    test_cursor_location(stmt, src, cursor_line, cursor_col);
+  }
+
   return stmt;
 }
 
@@ -509,10 +1001,32 @@ int main(int argc, char **argv) {
 
   struct ModuleStatementNode *new_root = NULL;
   struct ModuleStatementNode *current = NULL;
+  uint16_t cursor_line = (uint16_t)-1;
+  uint16_t cursor_col = (uint16_t)-1;
+
+  int file_start_idx = 1;
   for (int i = 1; i < (int)argc; i++) {
+    if (strcmp(argv[i], "--cursor") == 0 && i + 1 < argc) {
+      char *cursor_arg = argv[++i];
+      char *colon = strchr(cursor_arg, ':');
+      if (colon) {
+        *colon = '\0';
+        cursor_line = (uint16_t)atoi(cursor_arg);
+        cursor_col = (uint16_t)atoi(colon + 1);
+        printf("Cursor query: line=%d, col=%d\n", cursor_line, cursor_col);
+        file_start_idx = i + 1;
+      } else {
+        fprintf(stderr, "Invalid cursor format. Expected: --cursor line:col\n");
+        return 1;
+      }
+      continue;
+    }
+  }
+
+  for (int i = file_start_idx; i < (int)argc; i++) {
     printf("parsing file (%d)->(%s)\n", i, argv[i]);
     struct ModuleStatementNode *source_root =
-        parse_source_file(&allocator, argv[i]);
+        parse_source_file(&allocator, argv[i], cursor_line, cursor_col);
 
     if (current != NULL) {
       current->next =
