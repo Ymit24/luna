651f7784d1a04e987c4218678e7df717fc7fa024
Christian Smith <chris@Christians-MacBook-Air.local>
2026-04-07 22:28:36 -0500

    feat: add test runner with example test

---
+++ software/luna/tests/main.test.c
@@ -0,0 +1,56 @@
+#include <assert.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int sum(int a, int b) { return a + b; }
+
+void test_sum_good(void) { assert(sum(2, 4) == 6); }
+
+void test_sum_bad(void) { assert(sum(2, 4) == 7); }
+
+int exec_test(void (*test)(void)) {
+  int stat;
+
+  if (fork() == 0) {
+    int dev_null = open("/dev/null", O_WRONLY);
+    if (dev_null != -1) {
+      dup2(dev_null, STDERR_FILENO);
+      close(dev_null);
+    }
+    test();
+    exit(0);
+    return 0;
+  } else {
+    wait(&stat);
+  }
+
+  return stat == 0;
+}
+
+static int PASS_COUNT = 0;
+static int TEST_COUNT = 0;
+
+void run_test(char *name, void (*test)(void)) {
+  TEST_COUNT++;
+  if (exec_test(test)) {
+    printf("[ PASSED ] %s\n", name);
+    PASS_COUNT++;
+  } else {
+    printf("[ FAILED ] %s\n", name);
+  }
+}
+
+void print_results(void) {
+  printf("\n\n%d passed, %d failed, %d total\n", PASS_COUNT, TEST_COUNT-PASS_COUNT, TEST_COUNT);
+}
+
+int main(void) {
+  run_test("Test sum good", &test_sum_good);
+  run_test("Test sum bad", &test_sum_bad);
+
+  print_results();
+  return 0;
+}
